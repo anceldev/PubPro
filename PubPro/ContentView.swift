b52316806c157a79b3b7e665176f24a14b371efc
Ancel Guarayo <aguarayo@gmail.com>
2024-08-16 13:27:16 +0200

    First commit

---
+++ PubPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PubPro
+//
+//  Created by Ancel Dev account on 16/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PubPro/PubProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PubProApp.swift
+//  PubPro
+//
+//  Created by Ancel Dev account on 16/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PubProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
